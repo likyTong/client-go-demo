0df330655a69353fc74b7273ca2145e0e4e35a75
liky <njleitong@163.com>
2022-09-23 18:20:44 +0800

    discovery client

---
+++ 04/discovery_client/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"k8s.io/client-go/discovery"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func main() {
+	config, err := clientcmd.BuildConfigFromFlags("", clientcmd.RecommendedHomeFile)
+	if err != nil {
+		panic(err)
+	}
+}
